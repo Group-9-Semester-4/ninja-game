afea11e21e345235ee02281d5efbb88290ad79b5
Erik <erik.petra@afinode.com>
2021-03-16 07:41:04 +0100

    Moved Game init logic

--- Assets/Scripts/DrawCards.cs
+++ Assets/Scripts/DrawCards.cs
@@ -14,12 +14,7 @@ public class DrawCards : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        APIClient.APIClient.Instance.InitGame();
         
-        // TODO: Add unwanted cards in separate step later
-        var game = APIClient.APIClient.Instance.StartGame(new List<Card>());
-        
-        Debug.Log("Game created "+ game.uuid);
     }
 
    
