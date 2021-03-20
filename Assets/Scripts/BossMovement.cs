996abf6ef7feb1a592fd8652fa336c0513e7a0cf
Greeblood <30478852+Greeblood@users.noreply.github.com>
2021-03-20 20:11:12 +0100

    only missing some hitboxes and shooting now

---
+++ Assets/Scripts/BossMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    public float speed = 0.05f;
+
+    //translation values ( current movement speed values)
+    public float x;
+    public float y;
+    public float z;
+
+
+    //world coordinate values
+    public float xPos;
+    public float yPos;
+    public float zPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(x + speed, y, z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if(transform.position.x > 9.5)
+        {
+
+        }
+        */
+    }
+}
