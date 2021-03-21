36acc94e253c4afb06d691ad693769217c011d7d
Greeblood <30478852+Greeblood@users.noreply.github.com>
2021-03-21 17:55:37 +0100

    shooting now works
    only missing ammo count and hit detection to achieve minimal viable product.

---
+++ Assets/Scripts/Boss/Shooting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public float bulletZ = 8f;
+    public float force = 50f;
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UnityEngine.Vector2 screenMousePos = Input.mousePosition;
+        UnityEngine.Vector2 mousePos = Camera.main.ScreenToWorldPoint(new UnityEngine.Vector3(screenMousePos.x, screenMousePos.y, bulletZ));
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject firedBullet = Instantiate(bullet, transform.position, UnityEngine.Quaternion.identity);
+
+            UnityEngine.Vector3 bulletDirection = new UnityEngine.Vector3(mousePos.x, mousePos.y, bulletZ) - transform.position;
+
+            firedBullet.GetComponent<Rigidbody>().velocity += force * bulletDirection;
+        }
+    }
+}
