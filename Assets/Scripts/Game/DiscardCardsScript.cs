3a1cd656d86c8e59cb9caf218ed5b5f747e7f8c0
Erik <erik.petra@afinode.com>
2021-04-04 17:32:11 +0200

    Changed logic the card set and discarding cards work

--- Assets/Scripts/Game/DiscardCardsScript.cs
+++ Assets/Scripts/Game/DiscardCardsScript.cs
@@ -9,20 +9,32 @@ using UnityEngine.UI;
 public class DiscardCardsScript : MonoBehaviour
 {
     public Transform CardContainer;
+    public Dropdown CardSetDropdown;
     public GameObject cardPrefab;
+
+    private List<CardSet> _cardSets;
     
-    // Start is called before the first frame update
     void Start()
     {
+        _cardSets = new List<CardSet>() { new CardSet() {cards = new List<Card>()} };
 
-        var routine = APIClient.Instance.GetAllCards(PopulateCards);
+        var cardSetRoutine = APIClient.Instance.GetCardSets(PopulateCardSets);
 
-        StartCoroutine(routine);
+        StartCoroutine(cardSetRoutine);
     }
 
     private void PopulateCards(List<Card> cards)
     {
+        var childCount = CardContainer.childCount;
+        
+        for (var i = childCount - 1; i >= 0; i--)
+        {
+            Destroy(CardContainer.GetChild(i).gameObject);
+        }
+        
         var y = 0;
+        
+        Debug.Log("Populating "+ cards.Count);
 
         foreach (var card in cards)
         {
@@ -39,26 +51,68 @@ public class DiscardCardsScript : MonoBehaviour
             var cardScript = (CardScript) newGameObject.GetComponent(typeof(CardScript));
             cardScript.card = card;
             
-            y += 1;
+            Debug.Log(SceneManager.GetActiveScene().name);
+            
+            // TODO: Fix scale after all screens are fixed
+            if (SceneManager.GetActiveScene().name == "DiscardScene")
+            {
+                y += 1;
+            }
+            else
+            {
+                y += 50;
+            }
         }
+    }
+
+    private void PopulateCardSets(List<CardSet> cardSets)
+    {
+        foreach (var cardSet in cardSets)
+        {
+            _cardSets.Add(cardSet);
+            
+            var optionData = new Dropdown.OptionData(cardSet.name);
+            CardSetDropdown.options.Add(optionData);
+        }
+    }
+
+    private CardSet getSelectedCardSet()
+    {
+        var index = CardSetDropdown.value;
+
+        return _cardSets[index];
+    }
+
+    public void onCardSetChange()
+    {
+        var cardSet = getSelectedCardSet();
+
+        PopulateCards(cardSet.cards);
     }
 
     public void Continue()
     {
+        var cardSet = getSelectedCardSet();
+
+        if (string.IsNullOrEmpty(cardSet.id))
+        {
+            return;
+        }
+        
         var toggles = CardContainer.GetComponentsInChildren<Toggle>();
 
         var excludedCards = new List<string>();
 
         foreach (var toggle in toggles)
         {
-            if (toggle.isOn)
+            if (!toggle.isOn)
             {
                 var cardScript = (CardScript) toggle.GetComponent(typeof(CardScript));
                 excludedCards.Add(cardScript.card.id);
             }
         }
 
-        var options = new GameStartParam {unwantedCards = excludedCards};
+        var options = new GameStartParam {unwantedCards = excludedCards, cardSetId = cardSet.id};
 
         StartCoroutine(APIClient.Instance.StartGame(options, resource =>
         {
