773703ff2355d7275641f7f8c460711bfb795eb4
Erik <erik.petra@afinode.com>
2021-04-21 08:44:09 +0200

    Added change color on hit

--- Assets/Scripts/Boss/HitDetect.cs
+++ Assets/Scripts/Boss/HitDetect.cs
@@ -24,15 +24,23 @@ public class HitDetect : MonoBehaviour
         bossScript.bossScore.TakeDamage(3); // Take damage to head
         bossScript.bossScore.TakeDamage(2); // Take damage to torso
 
-
-        StartCoroutine(ColorChangeTime());
+        StartCoroutine(ColorChangeTime(bossScript));
     }
 
-    IEnumerator ColorChangeTime()
+    IEnumerator ColorChangeTime(Boss bossScript)
     {
+        // Change color to red
+        bossScript.GetComponent<Renderer>().material.color = new Color(0.5f, 0f, 0f);
+        
+        // Wait one second
+        yield return new WaitForSecondsRealtime(0.1F);
 
+        // change color back to normal
+        bossScript.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f);
+        
+        Destroy(gameObject);
     }
-
+    
     private void Stick()
     {
         thisProjectile.constraints = RigidbodyConstraints.FreezeAll;
